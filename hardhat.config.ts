8b53c5de22f8b7bec3a13fa56cd29eea762bf968
TheGreatAxios <sawyer@dirtroad.dev>
2022-11-02 17:00:22 -0700

    Automatic Verifcation Complete

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
 
@@ -19,6 +20,21 @@ const config: HardhatUserConfig = {
       url: process.env.RPC_URL as string,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY as string] /// Should Not Contain 0x at the start
     },
+  },
+  etherscan: {
+    apiKey: {
+        SKALE: "this-value-does-not-matter"
+    },
+    customChains: [
+        {
+            network: "SKALE",
+            chainId: Number(process.env.CHAIN_ID),
+            urls: {
+                apiURL: process.env.API_URL as string,
+                browserURL: process.env.BROWSER_URL as string
+            }
+        }
+    ]
   }
 };
 
